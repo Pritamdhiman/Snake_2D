4467d0b57e4cbb3fd75d34a9ea599456b9ca4b4c
Pritam <dhimanpritam1579@gmail.com>
2025-04-17 21:38:14 +0530

    removed unwanted imports and commented out code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-import javax.swing.*;
-import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
-//        game_panel.startGame();
         Game game = new Game();
     }
 }
